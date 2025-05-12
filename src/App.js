41dbc8976ebb4a0d7157d53fee766202afd731d7
jueunkordev <jueunkordev@gmail.com>
2025-05-12 11:33:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from './logo.svg'; // src라는 디렉토리에 이미지가 있을 경우
+import logo2 from './logo2.jpg';
+import './App.css?v=1';
+// className : HTML에서 class 속성과 동일한 속성을 말합니다.
+export default function App() { // 함수에 export를 이용하여 출력도 가능
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box"></div>
+      <img src={logo2} className="App-logo" alt="logo" />
     </div>
   );
 }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // react 라이브러리 선언
+import ReactDOM from 'react-dom/client'; // HTML, XML 출력
+import './index.css'; // css (Style 적용)
+import App from './App'; // Module을 로드하는 방식
+// import reportWebVitals from './reportWebVitals'; // 버그 및 데이터 상황에 대한 report를 확인하기 위함
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('webmain'));
 root.render(
+  /* root.render : Controller에서 해당 라이브러리에 대한 태그 기준으로 Model과 같은 역할을 합니다. */
   <React.StrictMode>
+    {/* <App /> : import 이름을 태그명으로 지정 */}
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
