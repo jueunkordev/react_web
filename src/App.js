62ec77f26f7083ef6c1992ebd3a66243526ff2b6
jueunkordev <jueunkordev@gmail.com>
2025-05-13 09:32:50 +0900

    250513 리액트 수업 내용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg'; // src라는 디렉토리에 이미지가 있을 경우
-import logo2 from './logo2.jpg';
+import logo1 from './logo.jpg';
 import './App.css?v=1';
 // className : HTML에서 class 속성과 동일한 속성을 말합니다.
 export default function App() { // 함수에 export를 이용하여 출력도 가능
   // export 사용 시 무조건 return 함수를 적용하여 사용해야 함
   /* return 함수 안에는 메인 태그 생성 후 publishing을 하게 됨 */
   // React는 return 함수에는 무조건 markup을 이용합니다. (XHTML 기준으로 사용됨)
+  // img src 사용 시 {} 시작하면 src 경로이며, ./로 시작하는 경로일 경우 public 디렉토리에서 가져옴
+  // require : src 경로에 있는 import 하지 않은 이미지를 가져올 떄 사용하빈다 그 외에 일반적인 <img src="./" /> 이미지는 모두 public 단, 암호화 불가능
   return (
     <>
       <input type="text" />
       <div className="box"></div>
-      <img src={logo2} className="App-logo" alt="logo" />
+      <img src="./logo1.jpg" /><br />
+      <img src={require('./logo2.jpg')} /><br />
+      <img src={logo1} className="App-logo" alt="logo" />
     </>
   );
 }
---
+++ src/Top.js
@@ -0,0 +1,31 @@
+import React from 'react'; // react 라이브러리 선언
+
+// 화살표 함수를 이용하여 export하는 형태
+var Top = () => {
+  return (
+    <>
+      <ul>
+        <li>메뉴 1</li>
+        <li>메뉴 2</li>
+        <li>메뉴 3</li>
+        <li>메뉴 4</li>
+      </ul>
+    </>
+  );
+}
+
+// 경력 없는 프론트
+// function Top() {
+//   return (
+//     <>
+//       <ul>
+//         <li>메뉴 1</li>
+//         <li>메뉴 2</li>
+//         <li>메뉴 3</li>
+//         <li>메뉴 4</li>
+//       </ul>
+//     </>
+//   );
+// }
+
+export default Top2;
